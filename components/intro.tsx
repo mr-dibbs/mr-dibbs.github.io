b007c962ada5f0fcac24f7ac187a8e7a2692b42e
mr-dibbs <miller.chris.david@gmail.com>
2023-03-26 21:22:13 -0500

    updates for first post

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,23 +1,26 @@
-import { CMS_NAME } from '../lib/constants'
+import { CMS_NAME } from "../lib/constants";
+import Image from "next/image";
+import logoMain from "/public/assets/blog/logos/mr-dibbs-logo.svg"
+import cn from 'classnames'
+
 
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        This Is Not A Drill
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and {CMS_NAME}.
+        <Image
+          src={logoMain}
+          alt="Main Logo"
+          className="hover:shadow-lg transition-shadow duration-200"
+          width={200}
+          height={200}
+        />
       </h4>
     </section>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
